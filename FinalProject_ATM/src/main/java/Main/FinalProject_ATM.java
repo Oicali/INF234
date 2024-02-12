208e292ebdb27890d0b2f01ad0f74852c223bea3
Oicali <121882893+Oicali@users.noreply.github.com>
2024-02-12 15:39:03 +0800

    Create new project

---
+++ FinalProject_ATM/src/main/java/Main/FinalProject_ATM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Main;
+
+/**
+ *
+ * @author jairus
+ */
+public class FinalProject_ATM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
